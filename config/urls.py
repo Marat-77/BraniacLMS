6cd9bc42e113dfe782ab665287f53ad0f4ee085b
zmd <maratz375@gmail.com>
2022-08-25 13:37:36 +0500

    Created mainapp/urls.py, edited config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from mainapp import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.hello_world),
-    path("<str:word>/", views.check_kwargs),
+    path("mainapp/", include("mainapp.urls")),
 ]
