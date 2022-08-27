abd804743ff9c3a5ecc1dcc9689c385ebde81e33
zmd <maratz375@gmail.com>
2022-08-27 20:03:59 +0500

    Addedd datetime_obj into /mainapp/views.py and added datetime_obj with filter into /mainapp/templates/mainapp/news.html

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,6 @@
 # Create your views here.
+from datetime import datetime
+
 from django.views.generic import TemplateView
 
 
@@ -10,12 +12,13 @@ class NewsPageView(TemplateView):
     template_name = 'mainapp/news.html'
 
     def get_context_data(self, **kwargs):
-        # Get all previous data
+        # Get all previous data from parent class
         context = super().get_context_data(**kwargs)
         # Create your own data
         context["news_title"] = "Громкий новостной заголовок"
         context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
         context["range"] = range(5)
+        context["datetime_obj"] = datetime.now()
         return context
 
 
