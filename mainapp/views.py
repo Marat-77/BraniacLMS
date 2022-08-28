93c0d406df54431c63ebb977e48e55be1cb6ca4e
zmd <maratz375@gmail.com>
2022-08-28 09:03:46 +0500

    Added load news from json-file

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,5 @@
 # Create your views here.
+import json
 from datetime import datetime
 
 from django.views.generic import TemplateView
@@ -15,17 +16,22 @@ class NewsPageView(TemplateView):
         # Get all previous data from parent class
         context = super().get_context_data(**kwargs)
         # Create your own data
-        context["news_title"] = "Громкий новостной заголовок"
-        context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
-        context["range"] = range(5)
-        context["datetime_obj"] = datetime.now()
+        context['news_title'] = 'Громкий новостной заголовок'
+        context['news_preview'] = 'Предварительное описание, которое заинтересует каждого'
+        context['range'] = range(5)
+        context['datetime_obj'] = datetime.now()
+        # добавил чтение новостей из файла json
+        # added load news from json-file
+        with open('mainapp/templates/mainapp/news.json', 'r', encoding='utf-8') as file:
+            news = json.load(file)
+            context['news'] = news
         return context
 
 
 class NewsWithPaginatorView(NewsPageView):
     def get_context_data(self, page, **kwargs):
         context = super().get_context_data(page=page, **kwargs)
-        context["page_num"] = page
+        context['page_num'] = page
         return context
 
 
